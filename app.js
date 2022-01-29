611f893e8691c34d0ee13695601c3452dbf9a9d7
David <dwoo1016@gmail.com>
2022-01-29 13:51:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { models: { User, Note }} = require('./db');
+const path = require('path');
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', async(req, res, next)=> {
+  try {
+    res.send({ token: await User.authenticate(req.body)});
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/auth', async(req, res, next)=> {
+  try {
+    res.send(await User.byToken(req.headers.authorization));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/users/:id/notes', async(req, res, next)=> {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      include: [Note]
+    });
+    res.send(user);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+// app.get('/notes', async(req, res, next)=> {
+//   try {
+//     res.send(await User.findAll());
+//   }
+//   catch(ex){
+//     next(ex);
+//   }
+// });
+
+app.use((err, req, res, next)=> {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
